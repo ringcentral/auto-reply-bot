19adda6d9d05be809053e0aef1ec8428ffbd1677
ZHAO Xudong <zxdong@gmail.com>
2022-03-22 08:54:17 +0800

    Fix lambda apps

--- src/server/app/handlers/maintain.js
+++
@@ -1,6 +0,0 @@
-import { invoke, getName } from './invoke'
-
-export function maintain (data) {
-  const name = getName()
-  return invoke(data, name)
-}
--- src/server/lambda.js
+++ src/server/lambda.js
@@ -5,13 +5,8 @@
 import serverlessHTTP from 'serverless-http'
 import app1 from './app/app'
 import axios from 'axios'
-import triggerFunc from './app/handlers/trigger'
 
 export const app = serverlessHTTP(app1)
-
-export const trigger = async (event) => {
-  return triggerFunc(event)
-}
 
 export const maintain = async () => {
   console.log('send renew request')
